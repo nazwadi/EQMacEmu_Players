236f6978b1f17bf8b0abdb3830e9d83eb2c2662a
Nazwadi <nazwadi@pm.me>
2024-03-03 12:44:16 -0500

    Add character lists to character transfer app, update stylesheet

--- character_transfer/views.py
+++ character_transfer/views.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db import connections
@@ -16,25 +17,35 @@ def index(request):
             forum_name = request.user.username
             ls_accounts = LoginServerAccounts.objects.filter(ForumName=forum_name)
             game_accounts = [Account.objects.filter(lsaccount_id=account.LoginServerID) for account in ls_accounts]
-            characters = list()
+            characters = dict()
             accounts = list()
             for account in game_accounts:
                 try:
-                    accounts.append(account.values('id', 'name')[0])
+                    accounts.append(account.values('id', 'name', 'time_creation', 'active')[0])
                     game_account_id = account.values('id')[0]['id']
                 except IndexError:
                     continue
                 if game_account_id is not None:
                     cursor = connections['game_database'].cursor()
                     cursor.execute(
-                        """SELECT cd.id, cd.name, a.id, a.Name 
+                        """SELECT cd.id, cd.name, cd.class, cd.race, cd.level, cd.zone_id, cd.x, cd.y, cd.z, a.id, a.Name, a.time_creation
                            FROM character_data as cd LEFT OUTER JOIN account as a ON cd.account_id = a.id 
                            WHERE cd.account_id  = '%s' 
                            ORDER BY cd.name;""", [game_account_id])
-                    result = cursor.fetchall()
+                    results = cursor.fetchall()
 
-                    for character in result:
-                        characters.append(character)
+                    Character = namedtuple("Character",
+                                           "char_id char_name char_class char_race char_level zone_id x y z"
+                                           " account_id account_name")
+                    for result in results:
+                        temp = Character(result[0], result[1], result[2], result[3],
+                                         result[4], result[5], result[6], result[7],
+                                         result[8], result[9], result[10])
+                        if temp.account_name not in characters:
+                            characters[temp.account_name] = dict()
+                            characters[temp.account_name]['time_creation'] = result[11]
+                            characters[temp.account_name]['characters'] = dict()
+                        characters[temp.account_name]['characters'][temp.char_name] = temp
 
             return render(request=request,
                           template_name="character_transfer/index.html",
