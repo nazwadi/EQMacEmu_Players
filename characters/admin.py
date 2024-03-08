7d78b133f1814ec50920707eb63db32aa5055a5d
Nazwadi <nazwadi@pm.me>
2024-03-08 00:02:37 -0500

    Add search filter to character admin page

--- characters/admin.py
+++ characters/admin.py
@@ -5,6 +5,8 @@ from .models import Characters
 
 
 class CharactersAdmin(admin.ModelAdmin):
+    search_fields = ["id", "name"]
+    list_display = ["id", "name", "class_name", "level"]
     list_filter = ["name"]
 
 
