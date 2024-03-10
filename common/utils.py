efad30bf70539c4932432f4fbd9a2d1da0a0fb06
Nazwadi <nazwadi@pm.me>
2024-03-10 16:09:07 -0400

    Attempt to fix case insensitive search

--- common/utils.py
+++ common/utils.py
@@ -10,12 +10,10 @@ def valid_game_account_owner(web_account: str, game_account_id: str) -> bool:
     :param game_account_id:
     :return: bool
     """
-    game_account = Account.objects.filter(id=game_account_id)
+    game_account = Account.objects.filter(id=game_account_id).first()
     ls_account = LoginServerAccounts.objects.filter(ForumName__iexact=web_account)
 
-    try:
-        game_account_name = game_account.values('name')[0]
-    except IndexError:
+    if game_account is None or ls_account is None:
         return False
 
     # Ensure the requested game account belongs to the current user
@@ -23,4 +21,4 @@ def valid_game_account_owner(web_account: str, game_account_id: str) -> bool:
     for account_name in ls_account.values('AccountName'):
         ls_account_names.append(account_name['AccountName'])
 
-    return game_account_name['name'] in ls_account_names
+    return game_account.name in ls_account_names
