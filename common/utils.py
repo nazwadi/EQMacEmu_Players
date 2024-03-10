5ef228bf75eb17959036e052a0bfa0999891f924
Nazwadi <nazwadi@pm.me>
2024-03-10 16:24:27 -0400

    lowercase loginserver account names before comparing to game account names

--- common/utils.py
+++ common/utils.py
@@ -18,7 +18,7 @@ def valid_game_account_owner(web_account: str, game_account_id: str) -> bool:
 
     # Ensure the requested game account belongs to the current user
     ls_account_names = []
-    for account_name in ls_account.values('AccountName'):
+    for account_name in ls_account.values('AccountName').lower():
         ls_account_names.append(account_name['AccountName'])
 
     return game_account.name in ls_account_names
