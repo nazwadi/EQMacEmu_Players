64c5cc62348150a75e38a134c84514991eb830ac
Nazwadi <nazwadi@pm.me>
2024-03-10 16:27:19 -0400

    Fix bug in lowercase logic

--- common/utils.py
+++ common/utils.py
@@ -18,7 +18,7 @@ def valid_game_account_owner(web_account: str, game_account_id: str) -> bool:
 
     # Ensure the requested game account belongs to the current user
     ls_account_names = []
-    for account_name in ls_account.values('AccountName').lower():
-        ls_account_names.append(account_name['AccountName'])
+    for account_name in ls_account.values('AccountName'):
+        ls_account_names.append(account_name['AccountName'].lower())
 
     return game_account.name in ls_account_names
