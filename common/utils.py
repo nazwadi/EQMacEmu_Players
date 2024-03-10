21a1a68259a3b6f8865bdfccd94111bf4fac6ec8
Nazwadi <nazwadi@pm.me>
2024-03-10 15:50:20 -0400

    Attempt to make account name comparisons case insensitive

--- common/utils.py
+++ common/utils.py
@@ -11,7 +11,7 @@ def valid_game_account_owner(web_account: str, game_account_id: str) -> bool:
     :return: bool
     """
     game_account = Account.objects.filter(id=game_account_id)
-    ls_account = LoginServerAccounts.objects.filter(ForumName=web_account)
+    ls_account = LoginServerAccounts.objects.filter(ForumName__iexact=web_account)
 
     try:
         game_account_name = game_account.values('name')[0]
