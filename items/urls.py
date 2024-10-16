cf4fff4a1941985198dc1230ab7907d01fb16d23
Nazwadi <nazwadi@pm.me>
2024-10-15 21:52:50 -0400

    enable item best in slot /items/bis URL

--- items/urls.py
+++ items/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("search", views.search, name="search"),
     path("discovered", views.discovered_items, name="discovered"),
     path("view/<int:item_id>", views.view_item, name="view"),
+    path("bis", views.best_in_slot, name="bis"),
 ]
