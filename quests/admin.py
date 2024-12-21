7a26e3ef8cd463052d8d994b467cc56d39a88167
Nazwadi <nazwadi@pm.me>
2024-12-21 11:42:48 -0500

    Reorganize quest admin page

--- quests/admin.py
+++ quests/admin.py
@@ -36,38 +36,34 @@ class QuestsAdmin(admin.ModelAdmin):
     filter_horizontal = ("quest_items", "related_npcs", "related_zones", "factions_required", "factions_raised", "factions_lowered")
     readonly_fields = ("id",)
     fieldsets = (
-        ("Quick Facts", {
-            "fields": ("id", "name", "starting_npc_id", "starting_zone", "expansion_introduced")
+        ("Where to Begin", {
+            "fields": ("id", "name", "starting_npc_id", "starting_zone")
+        }),
+        ("Availability", {
+            "classes": ("collapse",),
+            "description": f"Set restrictions to -1 to indicate no restrictions. (i.e. None). Setting Max Level to -1 will display the default Server Max Level, {SERVER_MAX_LEVEL}.",
+            "fields": ("expansion_introduced", "minimum_level", "maximum_level", "class_restrictions",
+                       "race_restrictions", "deity_restrictions", "is_repeatable", "monster_mission"),
         }),
         ("Description", {
             "description": "Put the content of the quest page here (i.e. quest description, walkthrough, checklists, notes)",
             "fields": ("description",),
         }),
-        ("Related NPCs", {
-            "description": "Include any npcs involved directly in quest dialogues or that drop quest items, etc...",
-            "fields": ("related_npcs",),
+        ("Quest Items", {
+            "description": "Any items that are required for this quest.",
+            "fields": ("quest_items",),
         }),
         ("Related Zones", {
             "description": "Include any zones related to this quest or that contain things required for this quest.",
             "fields": ("related_zones",),
         }),
-        ("Quest Items", {
-            "description": "Any items that are required for this quest.",
-            "fields": ("quest_items",),
+        ("Related NPCs", {
+            "description": "Include any npcs involved directly in quest dialogues or that drop quest items, etc...",
+            "fields": ("related_npcs",),
         }),
         ("Factions", {
             "classes": ("collapse",),
             "fields": ("factions_required", "factions_raised", "factions_lowered")
-        }),
-        ("Restrictions", {
-            "classes": ("collapse",),
-            "description": "Set these to -1 to indicate no restrictions. (i.e. None)",
-            "fields": ("class_restrictions", "race_restrictions", "deity_restrictions"),
-        }),
-        ("Advanced Settings", {
-            "classes": ("collapse",),
-            "description": f"Setting Max Level to -1 will display the default Server Max Level, {SERVER_MAX_LEVEL}.",
-            "fields": ("minimum_level", "maximum_level", "is_repeatable", "monster_mission"),
         }),
     )
 
