8653af1ee48d81f717226589b92981b9ae752f17
Nazwadi <nazwadi@pm.me>
2025-03-02 11:59:57 -0500

    Improve list display of character admin

--- characters/admin.py
+++ characters/admin.py
@@ -6,14 +6,19 @@ from .templatetags.data_utilities import player_class  # Import your filter func
 
 
 class CharactersAdmin(admin.ModelAdmin):
-    search_fields = ["id", "name"]
-    list_display = ["id", "name", "get_class_name", "level"]
+    search_fields = ["id", "name", "account_id"]
+    list_display = ["name", "get_character_id", "account_id", "get_class_name", "level"]
     list_filter = ["name"]
+
+    def get_character_id(self, obj):
+        return obj.id
 
     def get_class_name(self, obj):
         """Apply the player_class filter to the class_name field"""
         return player_class(obj.class_name)
 
+    get_character_id.short_description = "Character ID"
+    get_character_id.admin_order_field = "id"
     get_class_name.short_description = 'Class'  # Sets the column header in the admin
     get_class_name.admin_order_field = 'class_name'  # Tells Django to sort by class_name when this column is clicked
 
