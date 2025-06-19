e2f137e00d06460adf2650f3d97bd3c575130413
nazwadi <nazwadi@pm.me>
2025-06-19 15:23:31 -0400

    Add robots.txt to project urls.py

--- EQMacEmu_Players/urls.py
+++ EQMacEmu_Players/urls.py
@@ -21,8 +21,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from quests.views import NPCLookupView
+from django.http import HttpResponse
 
 admin.site.site_header = "EQMacEmu Accounts Administration"
+
+def robots_txt(request):
+    lines = [
+        "User-agent: *",
+        "Allow: /",
+        # Add any directories you want to disallow:
+        # "Disallow: /admin/",
+        # "Disallow: /api/",
+    ]
+    return HttpResponse("\n".join(lines), content_type="text/plain")
 
 urlpatterns = [
     path(r"", lambda request: redirect("/accounts/")),
@@ -42,7 +53,8 @@ urlpatterns = [
     path("recipes/", include("recipes.urls")),
     path("spells/", include("spells.urls")),
     path("zones/", include("zones.urls")),
-    path("mdeditor/", include('mdeditor.urls'))
+    path("mdeditor/", include('mdeditor.urls')),
+    path('robots.txt', robots_txt),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
