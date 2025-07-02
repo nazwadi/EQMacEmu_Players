17f69d2e9f4aa16dc4df429e299069f41d74cdce
nazwadi <nazwadi@pm.me>
2025-07-01 21:36:32 -0400

    Fix 500 error when spells are requested that don't exist

--- spells/views.py
+++ spells/views.py
@@ -3,6 +3,8 @@ from django.http import Http404
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import F
+from django.views.decorators.http import require_http_methods
+
 from common.models.spells import SpellsNew
 from common.models.items import Items
 from spells.utils import calc_buff_duration
@@ -10,6 +12,7 @@ from spells.utils import prep_spell_data
 from django.db.models import Q
 
 
+@require_http_methods(["GET"])
 def index(request):
     """
     Defines view for https://url.tld/spells/
@@ -19,13 +22,13 @@ def index(request):
     :param request: Http request
     :return: an Http Response object
     """
-    if request.method == "GET":
-        return render(request=request,
-                      template_name="spells/spells_index.html",
-                      context={},
-                      )
+    return render(request=request,
+                  template_name="spells/spells_index.html",
+                  context={},
+                  )
 
 
+@require_http_methods(["GET", "POST"])
 def search(request):
     """
     Search for a spell by name at https://url.tld/spells/search
@@ -103,6 +106,7 @@ def search(request):
                       )
 
 
+@require_http_methods(["GET"])
 def list_spells(request, class_id):
     """
     Defines view for https://url.tld/spells/list/<int:pk>
@@ -111,62 +115,62 @@ def list_spells(request, class_id):
     :param class_id: a class id field unique identifier
     :return: Http response
     """
-    if request.method == "GET":
-        clsid = class_id if class_id in [2, 3, 4, 5, 6, 8, 10, 11, 12, 13, 14, 15] else 0
-        match clsid:
-            case 2:
-                result = (SpellsNew.objects.filter(classes2__lt=255).filter(not_player_spell=0)
-                          .annotate(level=F('classes2')).order_by(f"classes{clsid}"))
-            case 3:
-                result = (SpellsNew.objects.filter(classes3__lt=255).filter(not_player_spell=0)
-                          .annotate(level=F('classes3')).order_by(f"classes{clsid}"))
-            case 4:
-                result = (SpellsNew.objects.filter(classes4__lt=255).filter(not_player_spell=0)
-                          .annotate(level=F('classes4')).order_by(f"classes{clsid}"))
-            case 5:
-                result = (SpellsNew.objects.filter(classes5__lt=255).filter(not_player_spell=0)
-                          .annotate(level=F('classes5')).order_by(f"classes{clsid}"))
-            case 6:
-                result = (SpellsNew.objects.filter(classes6__lt=255).filter(not_player_spell=0)
-                          .annotate(level=F('classes6')).order_by(f"classes{clsid}"))
-            case 8:
-                result = (SpellsNew.objects.filter(classes8__lt=255).filter(not_player_spell=0)
-                          .annotate(level=F('classes8')).order_by(f"classes{clsid}"))
-            case 10:
-                result = (SpellsNew.objects.filter(classes10__lt=255).filter(not_player_spell=0)
-                          .annotate(level=F('classes10')).order_by(f"classes{clsid}"))
-            case 11:
-                result = (SpellsNew.objects.filter(classes11__lt=255).filter(not_player_spell=0)
-                          .annotate(level=F('classes11')).order_by(f"classes{clsid}"))
-            case 12:
-                result = (SpellsNew.objects.filter(classes12__lt=255).filter(not_player_spell=0)
-                          .annotate(level=F('classes12')).order_by(f"classes{clsid}"))
-            case 13:
-                result = (SpellsNew.objects.filter(classes13__lt=255).filter(not_player_spell=0)
-                          .annotate(level=F('classes13')).order_by(f"classes{clsid}"))
-            case 14:
-                result = (SpellsNew.objects.filter(classes14__lt=255).filter(not_player_spell=0)
-                          .annotate(level=F('classes14')).order_by(f"classes{clsid}"))
-            case 15:
-                result = (SpellsNew.objects.filter(classes15__lt=255).filter(not_player_spell=0)
-                          .annotate(level=F('classes15')).order_by(f"classes{clsid}"))
-            case _:
-                result = None
-        spells = dict()
-        if result is not None:
-            for spell in result:
-                spell_effects = prep_spell_data(spell)
-                if spell.level in spells:
-                    spells[spell.level].append({"spell": spell, "spell_effects": spell_effects})
-                else:
-                    spells[spell.level] = [{"spell": spell, "spell_effects": spell_effects}]
-        return render(request=request,
-                      template_name="spells/list.html",
-                      context={"class_id": clsid,
-                               "spells": spells},
-                      )
+    clsid = class_id if class_id in [2, 3, 4, 5, 6, 8, 10, 11, 12, 13, 14, 15] else 0
+    match clsid:
+        case 2:
+            result = (SpellsNew.objects.filter(classes2__lt=255).filter(not_player_spell=0)
+                      .annotate(level=F('classes2')).order_by(f"classes{clsid}"))
+        case 3:
+            result = (SpellsNew.objects.filter(classes3__lt=255).filter(not_player_spell=0)
+                      .annotate(level=F('classes3')).order_by(f"classes{clsid}"))
+        case 4:
+            result = (SpellsNew.objects.filter(classes4__lt=255).filter(not_player_spell=0)
+                      .annotate(level=F('classes4')).order_by(f"classes{clsid}"))
+        case 5:
+            result = (SpellsNew.objects.filter(classes5__lt=255).filter(not_player_spell=0)
+                      .annotate(level=F('classes5')).order_by(f"classes{clsid}"))
+        case 6:
+            result = (SpellsNew.objects.filter(classes6__lt=255).filter(not_player_spell=0)
+                      .annotate(level=F('classes6')).order_by(f"classes{clsid}"))
+        case 8:
+            result = (SpellsNew.objects.filter(classes8__lt=255).filter(not_player_spell=0)
+                      .annotate(level=F('classes8')).order_by(f"classes{clsid}"))
+        case 10:
+            result = (SpellsNew.objects.filter(classes10__lt=255).filter(not_player_spell=0)
+                      .annotate(level=F('classes10')).order_by(f"classes{clsid}"))
+        case 11:
+            result = (SpellsNew.objects.filter(classes11__lt=255).filter(not_player_spell=0)
+                      .annotate(level=F('classes11')).order_by(f"classes{clsid}"))
+        case 12:
+            result = (SpellsNew.objects.filter(classes12__lt=255).filter(not_player_spell=0)
+                      .annotate(level=F('classes12')).order_by(f"classes{clsid}"))
+        case 13:
+            result = (SpellsNew.objects.filter(classes13__lt=255).filter(not_player_spell=0)
+                      .annotate(level=F('classes13')).order_by(f"classes{clsid}"))
+        case 14:
+            result = (SpellsNew.objects.filter(classes14__lt=255).filter(not_player_spell=0)
+                      .annotate(level=F('classes14')).order_by(f"classes{clsid}"))
+        case 15:
+            result = (SpellsNew.objects.filter(classes15__lt=255).filter(not_player_spell=0)
+                      .annotate(level=F('classes15')).order_by(f"classes{clsid}"))
+        case _:
+            result = None
+    spells = dict()
+    if result is not None:
+        for spell in result:
+            spell_effects = prep_spell_data(spell)
+            if spell.level in spells:
+                spells[spell.level].append({"spell": spell, "spell_effects": spell_effects})
+            else:
+                spells[spell.level] = [{"spell": spell, "spell_effects": spell_effects}]
+    return render(request=request,
+                  template_name="spells/list.html",
+                  context={"class_id": clsid,
+                           "spells": spells},
+                  )
 
 
+@require_http_methods(["GET"])
 def buy_spells(request, class_id):
     """
     Defines view for https://url.tld/spells/buy/<int:pk>
@@ -175,36 +179,36 @@ def buy_spells(request, class_id):
     :param class_id: a class id field unique identifier
     :return: Http response
     """
-    if request.method == "GET":
-        clsid = str(class_id) if class_id in [2, 3, 4, 5, 6, 8, 10, 11, 12, 13, 14, 15] else None
-        filename = f'static/spell_data/spell_buy.json'
-        with open(filename, 'r') as json_file:
-            spell_list = json.load(json_file)
-        try:
-            spells = spell_list[clsid]
-        except KeyError:
-            raise Http404("Sorry, that class id doesn't exist, or the class doesn't cast spells.")
+    clsid = str(class_id) if class_id in [2, 3, 4, 5, 6, 8, 10, 11, 12, 13, 14, 15] else None
+    filename = f'static/spell_data/spell_buy.json'
+    with open(filename, 'r') as json_file:
+        spell_list = json.load(json_file)
+    try:
+        spells = spell_list[clsid]
+    except KeyError:
+        raise Http404("Sorry, that class id doesn't exist, or the class doesn't cast spells.")
 
-        for spell in spells:
-            if spell['purchase_location_info'] != "None":
-                grouped_locations = {}
-                purchase_locations = spell['purchase_location_info'].split(';')
-                for location in purchase_locations:
-                    merchant = location.split(',')
-                    zone_key = merchant[3]
-                    merchant_id = merchant[0].replace(' ', '')
-                    merchant_info = merchant[1]
-                    if zone_key not in grouped_locations:
-                        grouped_locations.update({zone_key: []})
-                    grouped_locations[zone_key].append({'id': merchant_id, 'info': merchant_info})
-                    spell['purchase_location_info'] = grouped_locations
-        return render(request=request,
-                      template_name="spells/buy.html",
-                      context={"class_id": clsid,
-                               "spells": spells},
-                      )
+    for spell in spells:
+        if spell['purchase_location_info'] != "None":
+            grouped_locations = {}
+            purchase_locations = spell['purchase_location_info'].split(';')
+            for location in purchase_locations:
+                merchant = location.split(',')
+                zone_key = merchant[3]
+                merchant_id = merchant[0].replace(' ', '')
+                merchant_info = merchant[1]
+                if zone_key not in grouped_locations:
+                    grouped_locations.update({zone_key: []})
+                grouped_locations[zone_key].append({'id': merchant_id, 'info': merchant_info})
+                spell['purchase_location_info'] = grouped_locations
+    return render(request=request,
+                  template_name="spells/buy.html",
+                  context={"class_id": clsid,
+                           "spells": spells},
+                  )
 
 
+@require_http_methods(["GET"])
 def view_spell(request, spell_id):
     """
     Defines view for https://url.tld/spells/view/<int:pk>
@@ -213,12 +217,11 @@ def view_spell(request, spell_id):
     :param spell_id: a spell id field unique identifier
     :return: Http response
     """
-    if request.method == "GET":
-        try:
-            spell_data = SpellsNew.objects.get(pk=spell_id)
-        except ObjectDoesNotExist:
-            spell_data = None
-
+    try:
+        spell_data = SpellsNew.objects.get(pk=spell_id)
+    except ObjectDoesNotExist:
+        raise Http404("Sorry, that spell doesn't exist.")
+    else:
         min_level = 65
         spell_min_duration = calc_buff_duration(min_level, spell_data.buff_duration_formula, spell_data.buff_duration)
         spell_min_time = spell_min_duration * 6
